7f2d72a15a4c440b0af505ec1f8b245ad6ccfd95
Professor Aécio <professoraecio@gmail.com>
2025-03-17 19:17:13 -0300

    Unity 3D 04 - Programando o Movimento do Player
    Unity 3D 04 - Programando o Movimento do Player

--- Zelda PROD/Assets/Scripts/PlayerController.cs
+++ Zelda PROD/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController controller;
+    [Header("Config Player")]
+    public float movementSpeed = 3f;
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        direction = new Vector3(horizontal,0,vertical).normalized;
+        controller.Move(direction * movementSpeed * Time.deltaTime);
     }
 }
